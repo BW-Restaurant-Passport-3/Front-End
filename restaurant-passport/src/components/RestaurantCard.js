91e6e3a16ef45e20e35cda69842f6d79b1846557
Ana Tulea <anatulea@MacBook-Pro-de-Ana.local>
2020-01-08 20:25:24 -0800

    added 'been there' icon

--- restaurant-passport/src/components/RestaurantCard.js
+++ restaurant-passport/src/components/RestaurantCard.js
@@ -3,7 +3,7 @@ import {
   Card, Button, CardImg, CardTitle, CardText, CardDeck,
   CardSubtitle, CardBody
 } from 'reactstrap';
-
+import image from './img/map.png';
 import "../index.css";
 
 export default function RestaurantCard(props) {
@@ -15,20 +15,24 @@ export default function RestaurantCard(props) {
               return(
                 <CardDeck >
                   <Card >
-                    <CardImg top width="100%" src="https://images.unsplash.com/photo-1555396273-367ea4eb4db5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80" alt="Card image cap" />
+                  {/* <CardImg top width="10%" src={image} alt="Restaurant Card image " /> */}
                     <CardBody>
-                      <CardTitle>Name: {item.name}</CardTitle>
+
+                    <div className="title-img">
+                      <CardTitle>Name: {item.city} </CardTitle>
+                      <img src={image} alt="Restaurant Card image "/>
+                    </div>
                       <CardSubtitle>City: {item.city}</CardSubtitle>
                       <CardText>Phone Number: {item.phone_number}</CardText>
                       <CardText>My rating: {item.rating}</CardText>
                       <CardText>Notes: {item.notes}</CardText>
-                      <CardText >Webpage {item.website}</CardText>
+                      <CardText >Webpage: {item.website}</CardText>
                       <Button src={item.website}>View more</Button>
                     </CardBody>
                   </Card>
                 </CardDeck>
               );
-          })}
+          })} 
       </div>
     )
 }
