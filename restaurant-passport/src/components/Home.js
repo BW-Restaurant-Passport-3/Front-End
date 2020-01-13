f8ba2fdd1d0fb303406bb5b780cdc4fdca4d9a1e
Ana Tulea <anatulea@MacBook-Pro-de-Ana.local>
2020-01-12 19:07:22 -0800

    added list

---
+++ restaurant-passport/src/components/Home.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function Home(props) {
+    return (
+      <div className="home-wrapper">
+  
+           <div className="about-app"> <h3>Any foodie worth their weight in salt should always have 
+              ideas of where to eat in their home city whenever the 
+              opportunity arises (date night, a rare free night, girls 
+              night, bachelor party, etc.). If you are tired of defaulting to
+               the same dive spot because of "decision fatigue" every week, 
+               RestaurantPassport is the solution. View the restaurants you've
+                been to, have suggestions  at your fingertips for any occasion
+                 and taste, and brag to your friends about all the cool eateries
+                  you've been to!</h3></div>
+        <div className="buttons-wrapper">
+          <button onClick={() => props.history.push("/form")} className="md-button">
+            Go to dashboard
+          </button>
+          <button onClick={() => props.history.push("/list")} className="md-button">
+            Go list
+          </button>
+        </div>
+      </div>
+    );
+  }
+export default Home;
